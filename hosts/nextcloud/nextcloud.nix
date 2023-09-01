e95ba19517ade5245ef8c514c2608fe15025df58
digital-brew <rafal+github@digitalbrew.io>
2023-09-01 23:52:02 +0100

    update nextcloud settings

--- hosts/nextcloud/nextcloud.nix
+++ hosts/nextcloud/nextcloud.nix
@@ -20,7 +20,7 @@
 
     # Setup Nextcloud virtual host to listen on ports
     virtualHosts = {
-      "cloud.digitalbrew.io" = {
+      ${config.services.nextcloud.hostName} = {
          ## Force HTTP redirect to HTTPS
          forceSSL = true;
          ## LetsEncrypt
@@ -36,31 +36,48 @@
   };
 
   services.nextcloud = {
-      enable = true;
-#      package = pkgs.nextcloud27;
-      hostName = "cloud.digitalbrew.io";
+    enable = true;
+    package = pkgs.nextcloud27;
+    hostName = "cloud.digitalbrew.io";
 
-      # Use HTTPS for links
-      https = true;
+    # Use HTTPS for links
+    https = true;
 
-      # Auto-update Nextcloud Apps
-      autoUpdateApps.enable = true;
-      # Set what time makes sense for you
-      autoUpdateApps.startAt = "03:00:00";
+    configureRedis = true;
 
-      config = {
-        # Further forces Nextcloud to use HTTPS
-        overwriteProtocol = "https";
+    # Auto-update Nextcloud Apps
+    autoUpdateApps.enable = true;
+    # Set what time makes sense for you
+    autoUpdateApps.startAt = "03:00:00";
 
-        # Nextcloud PostegreSQL database configuration, recommended over using SQLite
-        dbtype = "mysql";
-        dbuser = "nextcloud";
-        dbhost = "localhost"; # nextcloud will add /.s.PGSQL.5432 by itself
-        dbname = "nextcloud";
-        dbpassFile = "/var/nextcloud/db-pass";
+    config = {
+      # Further forces Nextcloud to use HTTPS
+      overwriteProtocol = "https";
 
-        adminpassFile = "/var/nextcloud/admin-pass";
-        adminuser = "admin";
-   };
+      # Nextcloud PostegreSQL database configuration, recommended over using SQLite
+      dbtype = "mysql";
+      dbuser = "nextcloud";
+      dbhost = "localhost"; # nextcloud will add /.s.PGSQL.5432 by itself
+      dbname = "nextcloud";
+      dbpassFile = "/var/nextcloud/db-pass";
+
+      adminpassFile = "/var/nextcloud/admin-pass";
+      adminuser = "admin";
+    };
+
+    extraApps = with config.services.nextcloud.package.packages.apps; {
+      inherit news contacts calendar tasks;
+    };
+#    extraApps = {
+#      mail = pkgs.fetchNextcloudApp rec {
+#        url = "https://github.com/nextcloud-releases/mail/releases/download/v1.14.1/mail-v1.14.1.tar.gz";
+#        sha256 = "sha256-sQUsYC3cco6fj9pF2l1NrCEhA3KJoOvJRhXvBlVpNqo=";
+#      };
+#      contacts = pkgs.fetchNextcloudApp rec {
+#        url = "https://github.com/nextcloud-releases/contacts/releases/download/v4.2.2/contacts-v4.2.2.tar.gz";
+#        sha256 = "sha256-eTc51pkg3OdHJB7X4/hD39Ce+9vKzw1nlJ7BhPOzdy0=";
+#      };
+#    };
+    extraAppsEnable = true;
   };
 }
