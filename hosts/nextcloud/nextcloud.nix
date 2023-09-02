d0f0128006de68494467ed88b8ad807dfa7e14f1
digital-brew <rafal+github@digitalbrew.io>
2023-09-02 22:44:17 +0100

    update db settings

--- hosts/nextcloud/nextcloud.nix
+++ hosts/nextcloud/nextcloud.nix
@@ -90,7 +90,7 @@
       # Nextcloud PostegreSQL database configuration, recommended over using SQLite
       dbtype = "mysql";
       dbuser = "nextcloud";
-      dbhost = "/run/mysql"; # nextcloud will add /.s.PGSQL.5432 by itself
+      dbhost = "/run/mysqld"; # nextcloud will add /.s.PGSQL.5432 by itself
       dbport = 3306;
       dbname = "nextcloud";
       dbpassFile = "/var/nextcloud/db-pass";
