99cf7f1ce06c8dc1ac8a4ed8a29ca796c28a5eed
digital-brew <rafal+github@digitalbrew.io>
2023-09-03 23:12:48 +0100

    update nextcloud settings

--- hosts/nextcloud/nextcloud.nix
+++ hosts/nextcloud/nextcloud.nix
@@ -3,18 +3,18 @@
 {
   services.nextcloud = {
     enable = true;
-#    home = "/srv/lib/nextcloud";
+    home = "/srv/lib/nextcloud";
     hostName = "cloud.digitalbrew.io";
     https = true;
     maxUploadSize = "1024M";
     config = {
       adminuser = "moonlander";
-      adminpassFile = toString /var/lib/nextcloud/admin-pass;
+      adminpassFile = toString /var/secrets/admin-pass;
       dbtype = "mysql";
       dbhost = "localhost";
       dbport = "3306";
       dbuser = "nextcloud";
-      dbpassFile = toString /var/lib/nextcloud/db-pass;
+      dbpassFile = toString /var/secrets/db-pass;
       dbname = "nextcloud";
       dbtableprefix = "oc_";
     };
