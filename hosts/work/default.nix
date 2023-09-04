503f9f863c7c02fab5417b65d8315b585e09781c
digital-brew <rafal+github@digitalbrew.io>
2023-09-04 10:59:30 +0100

    add qemu module

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -12,6 +12,7 @@
     # Apple sound setting patch
     [(import ../../modules/nixos/firmware/work/sound/pipewire_sink_conf.nix)] ++
     [(import ../../modules/nixos/desktop/gnome.nix)] ++
+    [(import ../../modules/nixos/services/qemu.nix)] ++
     [(import ./configuration.nix)] ++
     [(import ./overlays.nix)] ++
     [(import ./hosts.nix)];
