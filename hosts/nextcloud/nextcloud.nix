9696c6815fcd99e27628441c75152c40eaf2b025
digital-brew <rafal+github@digitalbrew.io>
2023-09-04 08:50:16 +0100

    add nextcloud mail server settings

--- hosts/nextcloud/nextcloud.nix
+++ hosts/nextcloud/nextcloud.nix
@@ -1,46 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-  systemd.services."nextcloud-setup" = {
-    requires = ["mysql.service"];
-    after = ["mysql.service"];
-  };
-
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "rafal@digitalbrew.io";
-  };
-
-  services.nginx = {
-    enable = true;
-    # Use recommended settings
-    recommendedGzipSettings = true;
-    recommendedOptimisation = true;
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
-    virtualHosts = {
-      ${config.services.nextcloud.hostName} = {
-        ## Force HTTP redirect to HTTPS
-        forceSSL = true;
-        ## LetsEncrypt
-        enableACME = true;
-      };
-    };
-  };
-
-  services.mysql = {
-    enable = true;
-    package = pkgs.mariadb;
-    ensureDatabases = [ "nextcloud" ];
-    ensureUsers = [
-      { name = "nextcloud";
-        ensurePermissions = {
-          "nextcloud.*" = "ALL PRIVILEGES";
-        };
-      }
-    ];
-  };
-
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud27;
@@ -77,6 +37,71 @@
       dbtype = "mysql";
       dbuser = "nextcloud";
       dbname = "nextcloud";
+    };
+  };
+
+  systemd.services."nextcloud-setup" = {
+    requires = ["mysql.service"];
+    after = ["mysql.service"];
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "rafal+nextcloud@digitalbrew.io";
+  };
+
+  services.nginx = {
+    enable = true;
+    # Use recommended settings
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts = {
+      ${config.services.nextcloud.hostName} = {
+        ## Force HTTP redirect to HTTPS
+        forceSSL = true;
+        ## LetsEncrypt
+        enableACME = true;
+      };
+    };
+  };
+
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+    ensureDatabases = [ "nextcloud" ];
+    ensureUsers = [
+      { name = "nextcloud";
+        ensurePermissions = {
+          "nextcloud.*" = "ALL PRIVILEGES";
+        };
+      }
+    ];
+  };
+
+ # Setup mail server
+  services.maddy = {
+    enable = true;
+    hostname = "${config.services.nextcloud.hostName}";
+    primaryDomain = "${config.services.nextcloud.hostName}";
+    # Disable any sender validation checks
+    config = lib.concatStrings (
+      builtins.match "(.*)authorize_sender.*identity\n[ ]+\}(.*)" options.services.maddy.config.default
+    );
+  };
+
+  # Configure local mail delivery
+  programs.msmtp = {
+    enable = true;
+    accounts.default = {
+      host = "${config.services.nextcloud.hostName}";
+      port = 587;
+      auth = "login";
+      tls = "off";
+      from = "rafal+nextcloud@digitalbrew.io";
+      user = "rafal+nextcloud@digitalbrew.io";
+      password = "GJTerbbYaLpf8gi8T7utMVePmqiHBiQ4P9iaQwFAK9ZYUrrB";
     };
   };
 }
