a43750c782b3522e1f41025d75a352240359f877
digital-brew <rafal+github@digitalbrew.io>
2023-12-17 14:03:11 +0000

    remove dotfiles dir

--- dotfiles/eww_old2/scripts/windows/rdp-start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-xfreerdp -grab-keyboard /v:192.168.122.14 /u:moonlander /p:f1ght3Rf1sh /size:100% /d: /dynamic-resolution /gfx-h264:avc444 +gfx-progressive &
--- dotfiles/scripts/filemanager.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-eww open --toggle  audio_menu -c ~/.config/eww/screencapture
